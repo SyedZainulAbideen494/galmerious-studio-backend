e799f5e7241bdb156cb2eecd8913b19f9b2fb44b
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-07-04 19:29:08 +0530

    m

--- index.js
+++ index.js
@@ -643,7 +643,7 @@ app.post('/glast/api/create_event', upload.single('eventImage'), (req, res) => {
   }
 
   // Insert data into calendar table
-  const insertQuery = 'INSERT INTO calander (event_name, date, event_time, event_location, event_type, image_name, active) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const insertQuery = 'INSERT INTO calander (event_name, date, event_time, event_location, event_type, image_path, active) VALUES (?, ?, ?, ?, ?, ?, ?)';
   connection.query(insertQuery, [eventName, eventDate, eventTime, eventLocation, eventType, eventImageName, '1'], (err, results) => {
     if (err) {
       console.error('Error inserting event:', err);
